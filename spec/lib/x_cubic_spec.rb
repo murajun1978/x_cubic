d3ee64f2e005b75566970dd2b1f7f584363cfe23
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2014-04-30 18:32:39 +0900

    add test and refactoring

--- spec/lib/x_cubic_spec.rb
+++ spec/lib/x_cubic_spec.rb
@@ -2,19 +2,19 @@ require 'cubic'
 
 describe Cubic do 
   let(:cubic){Cubic.new(arg)}
-  
-  context "when argument is 2" do
-    let(:arg){"2\n"}
-    it "returns 8" do
-      expect(cubic.calculation).to eq 8
+
+  shared_examples_for "argument is Numerics" do |arg, collect|
+    let(:arg){"#{arg}\n"}
+    it "returns #{collect}" do
+      expect(cubic.calculation).to eq collect
     end
   end
 
-  context "when argument is ２" do
-    let(:arg){"２\n"}
-    it "returns 8" do
-      expect(cubic.calculation).to eq 8
-    end
+  describe "calculation" do
+    it_should_behave_like "argument is Numerics", 2, 8
+    it_should_behave_like "argument is Numerics", 10, 1000
+    it_should_behave_like "argument is Numerics", "２", 8
+    it_should_behave_like "argument is Numerics", "１０", 1000
   end
 
   context "when argument is e" do
